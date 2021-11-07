1dd2db25e707280bd48a29c317a6c08915f9b262
emircin <93774678+emircin@users.noreply.github.com>
2021-11-07 20:03:29 +0300

    Update Covid_19_Risk.py

--- Covid_19_Risk.py
+++ Covid_19_Risk.py
@@ -3,5 +3,5 @@ chronic = input("Do you have a severe chronic disease? ").capitalize()
 immune = input("Is your immune system too weak? ").capitalize()
 if age == "Yes" and chronic == "Yes" and immune == "Yes":
       print("You are in risky group")
-  else:
-        print("You are not in risky group")
+else:
+      print("You are not in risky group")
